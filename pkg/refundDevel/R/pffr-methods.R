c2a3c88e1ae5de5e49589e139e26790ab906c0b4
fabians <fabians@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-21 15:48:48 +0000

    pffr: doc fixes etc.; fpca.sc: set slow examples to "dontrun" to get rid of CRAN warnings

--- pkg/refundDevel/R/pffr-methods.R
+++ pkg/refundDevel/R/pffr-methods.R
@@ -60,7 +60,11 @@ predict.pffr <- function(object,
     ## warn if any entries in ... are not arguments for predict.gam 
     dots <- list(...)
     if(length(dots)){
-        validDots <- names(formals(predict.gam))
+        validDots <- c(names(formals(predict.gam)), "cluster")
+          # should be 
+          # unique(c(names(formals(predict.gam)), 
+          #          names(formals(predict.bam)))) 
+          # but predict.bam is not exported.
         notUsed <- names(dots)[!(names(dots) %in% validDots)]
         if(length(notUsed))
             warning("Arguments <", paste(notUsed, collapse=", "), "> supplied but not used." )
@@ -195,7 +199,9 @@ predict.pffr <- function(object,
     
     
     #call predict.gam
-    call[[1]] <- mgcv::predict.gam
+    call[[1]] <- if(inherits(object, "bam")){
+        mgcv:::predict.bam  
+    }  else mgcv::predict.gam
     call$object <- as.name("object")
     ret <- eval(call)
     
@@ -346,6 +352,7 @@ residuals.pffr <- function (object, reformat=TRUE, ...)
 #'  or vector (if \code{!reformat}) of fitted values 
 #' @method fitted pffr
 #' @author Fabian Scheipl
+#  TODO: fix for non-grid data without newdata, yind
 fitted.pffr <- function (object, reformat=TRUE, ...) 
 {
     if (!inherits(object, "pffr")) 
