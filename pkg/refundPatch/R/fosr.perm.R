b3094a2171f1e1debad0f0ac7eb96cc8401b5f1b
fabian-s <fabian.scheipl@googlemail.com>
2015-02-26 11:20:32 +0100

    redo folder structure

--- pkg/refundPatch/R/fosr.perm.R
+++
@@ -1,9 +0,0 @@
-fosr.perm <-
-function(Y=NULL, fdobj=NULL, X, con=NULL, X0=NULL, con0=NULL, argvals = NULL,   
-lambda=NULL, lambda0=NULL, multi.sp=FALSE, nperm, level=.05, plot=TRUE, xlabel="", title=NULL, prelim=15, ...) {
-    fpobj1 = fosr.perm.fit(Y=Y, fdobj=fdobj, X=X, con=con, X0=X0, con0=con0, argvals=argvals, lambda=lambda, lambda0=lambda0, multi.sp=multi.sp, nperm=nperm, prelim=prelim, ...)
-    fpobj2 = fosr.perm.test(fpobj1, level=level)
-    if (plot) plot(fpobj2, xlabel=xlabel, title=title)
-    fpobj2
-}
-
