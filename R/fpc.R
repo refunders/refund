6d8dab040a95e31f30b3da653de20c9072e8f992
jgellar <jongellar@gmail.com>
2015-06-29 09:26:22 -0400

    Removes pfr.xval (which is still being developed) in preparation for the merge with pfr-devel. Also updates some of the fpc examples.

--- R/fpc.R
+++ R/fpc.R
@@ -54,16 +54,22 @@
 #' # Fit PFCR_R
 #' gasmod1 <- pfr(octane ~ fpc(NIR, ncomp=30), data=gasoline)
 #' plot(gasmod1, rug=FALSE)
-#' est <- coef(gasmod)
+#' est1 <- coef(gasmod1)
 #' 
 #' # Fit FPCR_C with fpca.sc
 #' gasmod2 <- pfr(octane ~ fpc(NIR, method="fpca.sc", ncomp=6), data=gasoline)
-#' plot(gasmod2, rug=FALSE, se=FALSE)
+#' plot(gasmod2, se=FALSE)
+#' est2 <- coef(gasmod2)
 #' 
 #' # Fit penalized model with fpca.face
 #' gasmod3 <- pfr(octane ~ fpc(NIR, method="fpca.face", penalize=TRUE), data=gasoline)
 #' plot(gasmod3, rug=FALSE)
+#' est3 <- coef(gasmod3)
 #' 
+#' par(mfrow=c(1,1))
+#' plot(value ~ X.argvals, type="l", data=est1, ylim=(range(est1$value)*1.35))
+#' lines(value ~ X.argvals, col=2, data=est2)
+#' lines(value ~ X.argvals, col=3, data=est3)
 #' 
 #' @seealso \code{\link{lf}}, \code{\link{smooth.construct.fpc.smooth.spec}}
 #' @author Jonathan Gellar \email{JGellar@@mathematica-mpr.com}
