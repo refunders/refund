b5199c3282c9aa00dd98abdc9353c7ac8466b6fb
jgellar <jongellar@gmail.com>
2015-07-01 14:36:23 -0400

    Re-writes coefficients.pfr to call plot.gam instead of mgcv:::plot.smooth.term, because CRAN doesn't like using :::. This simplifies the code and makes it so some of the arguments can be moved into "...". Only problem is I can't turn off plotting!

--- R/coefficients.pfr.R
+++ R/coefficients.pfr.R
@@ -1,88 +1,68 @@
 #' Extract coefficient functions from a fitted pfr-object
 #' 
 #' @param object return object from \code{\link{pfr}}
-#' @param which which terms to extract coefficients from (integer vector), defaults
-#'    to all terms
-#' @param n see \code{\link[mgcv]{plot.gam}}
-#' @param n2 see \code{\link[mgcv]{plot.gam}}
+#' @param which which of the smooth terms to extract coefficients from
+#'    (integer vector). If \code{NULL}, returns all smooth terms.
 #' @param se compute standard errors? defaults to TRUE. See \code{\link[mgcv]{plot.gam}}
-#' @param seWithMean see \code{\link[mgcv]{plot.gam}}
 #' @param exclude if \code{TRUE}, excludes reporting of the estimate at coordinates that are
 #'   "too far" from data used to fit the model, as determined by
 #'   \code{mgcv::plot.mgcv.smooth}, by setting the estimate to \code{NA}.
-#' @param ... further arguments passed on to \pkg{mgcv}'s \code{plot.mgcv.smooth}
+#' @param ... further arguments passed on to \pkg{mgcv}'s \code{plot.gam}. Common
+#'   arguments include \code{n} and \code{n2} to set the number of coordinates
+#'   to estimate for 1-D and 2-D coefficient functions, and \code{seWithMean}
+#'   if the standard error should include uncertainty about the overall mean.
+#'   See \code{\link[mgcv]{plot.gam}}.
 #' @return a list of data.frames containing the evaluation points, 
-#'    coefficient function values and optionally their se's for each term in \code{which}
-#' @author Fabian Scheipl, se-computation parts adapted from Simon Wood's \code{plot.gam}.
+#'    coefficient function values and optionally their se's for each term in \code{which}.
+#'    If only one term is selected, the one data frame is unlisted.
+#' @author Fabian Scheipl and Jonathan Gellar, se-computation parts adapted from
+#'    Simon Wood's \code{plot.gam}.
 #' @examples 
-#' #TODO: see pfr 
+#' #TODO: see ?pfr 
 #' @export
-coefficients.pfr <- function(object, which, n=100, n2=40, se=TRUE, 
-                             seWithMean=TRUE, exclude=FALSE, ...){
-  if (missing(which)) {
-    which <- seq_along(object$smooth)
-  } 
-  coef <- vector(length(which), mode="list")
-  for (i in which) {
-    is.re <- "random.effect" %in% class(object$smooth[[i]])
-    plotf <- if(is.re) {
-      mgcv:::plot.random.effect
-    } else {
-      mgcv:::plot.mgcv.smooth
-    }
-    plotdata <- plotf(object$smooth[[i]], P=NULL, n=n, 
-                                         n2=n2, data = object$model, ...)
-    if(is.re) plotdata$x <- levels(object$model[[object$smooth[[i]]$term]])
-    first <- object$smooth[[i]]$first.para
-    last <- object$smooth[[i]]$last.para
-    coef[[i]] <- list()
-    coef[[i]]$value <- drop(plotdata$X %*% object$coefficients[first:last])
+
+coefficients.pfr <- function(object, which=NULL, se=TRUE, exclude=FALSE, ...) {
+  
+  # If (se==TRUE), replace with 1 (indicating we want 1 SE returned by plot.gam)
+  if (is.logical(se)) if (se) se <- 1
+  
+  # exclude sets the too.far value appropriately
+  too.far <- list(...)$too.far
+  too.far <- ifelse(is.null(too.far), ifelse(exclude, 0.1, 0), too.far)
+  
+  plotdata <- mgcv::plot.gam(object, select=which, se=se, too.far=too.far, ...)
+  
+  coef <- lapply(1:length(plotdata), function(i) {
+    pd <- plotdata[[i]]
     
-    # ctrl-c-v from plot.mgcv.smooth :
-    if (exclude & !is.null(plotdata$exclude))
-      coef[[i]]$value[plotdata$exclude] <- NA
-    if (se && plotdata$se) { ## get standard errors for fit
-      ## test whether mean variability to be added to variability (only for centred terms)
-      if (seWithMean && attr(object$smooth[[i]], "nCons") > 0) {
-        if (length(object$cmX) < ncol(object$Vp)){
-          object$cmX <- c(object$cmX,rep(0,ncol(object$Vp)-length(object$cmX)))
-        } 
-        X1 <- matrix(object$cmX, nrow(plotdata$X), ncol(object$Vp), byrow=TRUE)
-        meanL1 <- object$smooth[[i]]$meanL1
-        if (!is.null(meanL1)) {
-          X1 <- X1 / meanL1
-        } 
-        X1[,first:last] <- plotdata$X
-        coef[[i]]$se <- sqrt(pmax(0,rowSums((X1 %*% object$Vp) * X1)))
-      } else {
-        coef[[i]]$se <- ## se in centred (or anyway unconstained) space only
-          sqrt(pmax(0, 
-                    rowSums((plotdata$X %*% 
-                               object$Vp[first:last, first:last, drop=FALSE]) * 
-                              plotdata$X)))
-      }
-      if (exclude & !is.null(plotdata$exclude)) {
-        coef[[i]]$se[plotdata$exclude] <- NA
-      }
-    }
-    if(object$smooth[[i]]$dim == 1) {
-      if(!is.re) {
-        coef[[i]][[gsub("tmat", "argvals", plotdata$xlab)]] <- plotdata$x
-      } else {
-        coef[[i]][[object$smooth[[i]]$term]] <- plotdata$x
-      }
-      
+    # Create coef.i with coordinates
+    is.re <- "random.effect" %in% class(object$smooth[[i]])
+    if(is.re) pd$x <- levels(object$model[[object$smooth[[i]]$term]])
+    
+    coef.i <- if(object$smooth[[i]]$dim == 1) {
+      setNames(data.frame(pd$x),
+               ifelse(is.re, object$smooth[[i]]$term,
+                      gsub("tmat", "argvals", pd$xlab)))
     } else {
-      grid <- expand.grid(x=plotdata$x, y=plotdata$y)
-      coef[[i]][[gsub("tmat", "argvals", plotdata$ylab)]] <- grid$y
-      coef[[i]][[gsub("\\.omat", "", plotdata$xlab)]] <- grid$x
+      grid <- expand.grid(x=pd$x, y=pd$y)
+      setNames(data.frame(grid$x, grid$y), 
+               c(gsub("\\.omat", "", pd$xlab),
+                 gsub("tmat", "argvals", pd$ylab)))
     }
-    coef[[i]] <- as.data.frame(coef[[i]])
-  } #end i in which
-  if(length(which) == 1) {
-    #special extra wish from Jon:
+    
+    # Add estimate & SE
+    coef.i$value <- pd$fit
+    if (se)
+      coef.i$se  <- pd$se
+    
+    coef.i
+  })
+  
+  #special extra wish from Jon:
+  if(length(coef) == 1) {
     coef[[1]]
   } else {
+    names(coef) <- sapply(object$smooth, function(x) x$label)
     coef
   }
 }
