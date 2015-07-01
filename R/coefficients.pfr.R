7ee9d3ba451810cd6a7ddded1cf719ba428e948e
Mathew W. McLean <mathew.w.mclean@gmail.com>
2015-07-01 16:01:35 -0500

    prevent coef. method from plotting

--- R/coefficients.pfr.R
+++ R/coefficients.pfr.R
@@ -1,5 +1,5 @@
 #' Extract coefficient functions from a fitted pfr-object
-#' 
+#'
 #' @param object return object from \code{\link{pfr}}
 #' @param which which of the smooth terms to extract coefficients from
 #'    (integer vector). If \code{NULL}, returns all smooth terms.
@@ -12,52 +12,58 @@
 #'   to estimate for 1-D and 2-D coefficient functions, and \code{seWithMean}
 #'   if the standard error should include uncertainty about the overall mean.
 #'   See \code{\link[mgcv]{plot.gam}}.
-#' @return a list of data.frames containing the evaluation points, 
+#' @return a list of data.frames containing the evaluation points,
 #'    coefficient function values and optionally their se's for each term in \code{which}.
 #'    If only one term is selected, the one data frame is unlisted.
 #' @author Fabian Scheipl and Jonathan Gellar, se-computation parts adapted from
 #'    Simon Wood's \code{plot.gam}.
-#' @examples 
-#' #TODO: see ?pfr 
+#' @examples
+#' #TODO: see ?pfr
 #' @export
 
 coefficients.pfr <- function(object, which=NULL, se=TRUE, exclude=FALSE, ...) {
-  
+
   # If (se==TRUE), replace with 1 (indicating we want 1 SE returned by plot.gam)
   if (is.logical(se)) if (se) se <- 1
-  
+
   # exclude sets the too.far value appropriately
   too.far <- list(...)$too.far
   too.far <- ifelse(is.null(too.far), ifelse(exclude, 0.1, 0), too.far)
-  
+
+  ## dump plots to file since can't pass type = "n" to plot.gam
+  tfile <- tempfile()
+  pdf(tfile)
   plotdata <- mgcv::plot.gam(object, select=which, se=se, too.far=too.far, ...)
-  
+  dev.off()
+  if (file.exists(tfile))
+      file.remove(tfile)
+
   coef <- lapply(1:length(plotdata), function(i) {
     pd <- plotdata[[i]]
-    
+
     # Create coef.i with coordinates
     is.re <- "random.effect" %in% class(object$smooth[[i]])
     if(is.re) pd$x <- levels(object$model[[object$smooth[[i]]$term]])
-    
+
     coef.i <- if(object$smooth[[i]]$dim == 1) {
       setNames(data.frame(pd$x),
                ifelse(is.re, object$smooth[[i]]$term,
                       gsub("tmat", "argvals", pd$xlab)))
     } else {
       grid <- expand.grid(x=pd$x, y=pd$y)
-      setNames(data.frame(grid$x, grid$y), 
+      setNames(data.frame(grid$x, grid$y),
                c(gsub("\\.omat", "", pd$xlab),
                  gsub("tmat", "argvals", pd$ylab)))
     }
-    
+
     # Add estimate & SE
     coef.i$value <- pd$fit
     if (se)
       coef.i$se  <- pd$se
-    
+
     coef.i
   })
-  
+
   #special extra wish from Jon:
   if(length(coef) == 1) {
     coef[[1]]
