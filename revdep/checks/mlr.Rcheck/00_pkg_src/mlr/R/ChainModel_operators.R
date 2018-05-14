0e724c8a95c6afd9b09dae12dcd0300d8cd93c64
Julia Wrobel <jw3134@cumc.columbia.edu>
2018-05-14 16:39:39 -0400

    remove reverse dependency checks

--- revdep/checks/mlr.Rcheck/00_pkg_src/mlr/R/ChainModel_operators.R
+++
@@ -1,5 +0,0 @@
-getLeafModel = function(model) {
-  if (inherits(model, "BaseWrapperModel"))
-    return(getLeafModel(model$learner.model$next.model))
-  return(model)
-}
--- revdep/checks/mlr.Rcheck/00_pkg_src/mlr/R/learners.R
+++
@@ -1,9 +0,0 @@
-#' @title List of supported learning algorithms.
-#'
-#' @description
-#' All supported learners can be found by [listLearners] or as a table
-#' in the tutorial appendix: <http://mlr-org.github.io/mlr-tutorial/release/html/integrated_learners/>.
-#'
-#' @name learners
-#' @rdname learners
-NULL
