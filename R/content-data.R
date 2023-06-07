d0e89d8b30bab71305de1e7c7ed0155ea13b76a4
Erjia Cui <churchillcui@gmail.com>
2023-06-07 10:05:01 -0400

    add content data

---
+++ R/content-data.R
@@ -0,0 +1,26 @@
+##' The CONTENT child growth study
+##'
+##' The CONTENT child growth study was funded by the Sixth Framework Programme 
+##' of the European Union, Project CONTENT (INCO-DEV-3-032136) and was led by 
+##' Dr. William Checkley. The study was conducted between May 2007 and February 
+##' 2011 in Las Pampas de San Juan Miraflores and Nuevo Paraiso, two peri-urban 
+##' shanty towns with high population density located on the southern edge of 
+##' Lima city in Peru.
+##'
+##' @name content
+##' @docType data
+##' @format A list made up of \describe{
+##' \item{id}{Numeric vector of subject ID numbers;}
+##' \item{ma1fe0}{Numeric vector of the sex of the child, 1 for male and 0 for female;}
+##' \item{weightkg}{Numeric vector of the weight of the child measured in kilograms(kg);}
+##' \item{height}{Numeric vector of the height of the child measured in centimeters;}
+##' \item{agedays}{Numeric vector of the age of the child measured in days;}
+##' \item{cbmi}{Numeric vector of the BMI of the child;}
+##' \item{zlen}{Numeric vector of the height-for-age z-scores;}
+##' \item{zwei}{Numeric vector of the weight-for-age z-scores;}
+##' \item{zwfl}{Numeric vector of the weight-for-height z-scores;}
+##' \item{zbmi}{Numeric vector of the BMI-for-age z-scores;}
+##' }
+##' @references Crainiceanu, C., Goldsmith, J., Leroux, A., Cui, E. (2023). Functional
+##' Data Analysis with R. \emph{Chapman \& Hall/CRC Statistics}
+NULL
